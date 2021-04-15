bd8bb0a82a023ae1d132572844b8fa821e3dbc11
ElmiraSinani <elmira.si19@gmail.com>
2021-04-14 18:07:01 -0700

    updated 1.1.1

--- src/first_month/Task 1.1.1 Python.py
+++ src/first_month/Task 1.1.1 Python.py
@@ -2,19 +2,21 @@
 base = 5;
 height = 10;
 area = (base*height)/2
-print("Area = ", area)
+print("#1 Area = ", area)
 
 #2. Input a natural number n and output its last digit.
 n = 589879
-print("Last digit = ", n % 10)
+print("#2 Last digit = ", n % 10)
 
 #3. Input a two-digit natural number and output the sum of its digits.
 num = 59
 digit1 = num//10
 digit2 = num%10
-str1 = str(59)
-print("Sum =", int(str1[0]) + int(str1[1]))
-print(digit1,"+",digit2, '=', digit1+digit2)
+print("#3 2. Sum =", digit1+digit2)
+
+str1 = str(num)
+print("#3 1. Sum =", int(str1[0]) + int(str1[1]))
+
 
 #4. You are given the first and second number in an arithmetic progression and natural number  n. Find n-th element of arithmetic progression.
 a1 = 5
@@ -22,11 +24,5 @@ a2 = 8
 d = a2-a1
 n = 57
 a57 = a1 + (n-1)*d
-print("a57 = ", a57)
+print("#4 a57 = ", a57)
 
-
-s = {1, 2, 3}
-l = ['a','b','c','f','c','a']
-
-print(l.index('c',2))
-#print(type(l))

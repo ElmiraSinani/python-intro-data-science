7543d0e8802d23e391d0b8b9a4c8ad4090f17dc3
ElmiraSinani <elmira.si19@gmail.com>
2021-04-18 20:36:45 -0700

    updated task 1.3.3 - added while and for loops

--- src/first_month/Task_1_3_3.py
+++ src/first_month/Task_1_3_3.py
@@ -5,18 +5,56 @@ def getFactorial(n):
     else:
         return n * getFactorial(n-1)
 
-calcFactorial = getFactorial(1)
-print("#1 getFactorial", calcFactorial)
+calcFactorial = getFactorial(6)
+print("#1.1 getFactorial", calcFactorial)
+
+def getFactorialFor(n):
+    f = 1
+    for i in range(1, n+1):
+        f = f * i
+    return f
+
+print("#1.2 getFactorialFor", getFactorialFor(6))
+
+def getFactorialWhile(n):
+    i = 1; f = 1;
+    while (i<=n):
+        f = f * i
+        i += 1
+    return f
+
+print("#1.3 getFactorialWhile", getFactorialWhile(5))
+
 
 #2. Write a Python function which returns the n-th element of the fibonacci series. Fn = Fn-1 + Fn-2
 def getFibonacciNum(n):
     if n<0:
         return False
     elif (n==1 or n==2):
-        return n-1
+        return 1
     else:
         return getFibonacciNum(n-1) + getFibonacciNum(n-2)
 
-calcFibonacci = getFibonacciNum(6)
-print("#2 getFibonacciNum", calcFibonacci)
+calcFibonacci = getFibonacciNum(9)
+print("#2.1 getFibonacciNum", calcFibonacci)
+
+def getFibonacciNumFor(n):
+    f = [0, 1]
+    for i in range(2, n + 1):
+        f.append(f[i - 1] + f[i - 2])
+    return f[n]
+
+print("#2.2 getFibonacciNumFor", getFibonacciNumFor(9))
+
+def getFibonacciNumWhile(n):
+    f = [0, 1]
+    i=2
+    while (i <= n):
+        f.append(f[i - 1] + f[i - 2])
+        i += 1
+    return f[n]
+
+print("#2.3 getFibonacciNumFor", getFibonacciNumWhile(9))
+
+
 

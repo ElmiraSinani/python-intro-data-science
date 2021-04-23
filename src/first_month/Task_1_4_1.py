f62ab0842abed3bd7dfdd78063ec23a557b2fcf6
ElmiraSinani <elmira.si19@gmail.com>
2021-04-23 11:06:34 -0700

    updated task 1.4.1 and 1.4.2

--- src/first_month/Task_1_4_1.py
+++ src/first_month/Task_1_4_1.py
@@ -2,36 +2,54 @@
 lst1 = [1,2,2,2,5,7,8,1,11]
 lst2 = [0,2,2,3,5,7,8,0,10]
 
-st = set(lst1)
-print("#1 ", st)
+def getListWithoutDuplicates(lst):
+    st = set(lst1)
+    return list(st)
+
+print("#1 ", getListWithoutDuplicates(lst1))
 
 #2. Գրել ծրագիր, որը տրված 2 ցուցակների համար կվերադարձնի նրանց ընդհանուր էլեմենտների բազմությունը
-st2 = set(lst1) & set(lst2)
-print("#2 ", st2)
+def getIntersection(lst1, lst2):
+    return set(lst1) & set(lst2)
+
+print("#2 ", getIntersection(lst1, lst2))
 
 #3. Գրել ծրագիր, որը տրված երկու ցուցակների համար կվերադարձնի այն էլեմենտները, որոնք կան առաջինում, բայց չկան երկրորդում
-st3 = set(lst1) - set(lst2)
-print("#3 ", st3)
+def getDifference(lst1, lst2):
+    return set(lst1) - set(lst2)
+
+print("#3 ", getDifference(lst1, lst2))
 
 #4. Գրել ծրագիր, որը տրված երկու ցուցակների համար կվերադարձնի այն էլեմենտները, որոնք կան կամ առաջինում, կամ երկրորդում,
 # բայց ոչ միաժամանակ երկուսում էլ։
-st4 = set(lst1) ^ set(lst2)
-print("#4 ", st4)
+def getSymmetricDifference(lst1, lst2):
+    return set(lst1) ^ set(lst2)
+
+print("#4 ", getSymmetricDifference(lst1, lst2))
 
 #5. Գրել ծրագիր, որը տրված երկու ցուցակների համար կվերադարձնի այն էլեմենտները, որոնք կան կամ առաջին ցուցակում,
 # կամ երկրորդ, կամ երկուսում միաժամանակ
-st5 = set(lst1) | set(lst2)
-print("#5 ", st5)
+def getUnion(lst1, lst2):
+    return set(lst1) | set(lst2)
+
+print("#5 ", getUnion(lst1, lst2))
 
 #6. Գրել ծրագիրը, որը կստանա բազմություն և էլեմենտի արժեք, ու բազմությունից կհեռացնի տվյալ արժեքով էլեմենտը։
-l = {'b','c','a','d'}
-l.remove('a')
-print("#6 ", l)
+def removeElemntFromSet(el, st):
+    st.remove(el)
+    return st
+
+st = {'b','c','a','d'}
+el = 'a'
+print("#6 ", removeElemntFromSet(el, st))
 
 #7. Գրել ծրագիր, որը կստանա թվերի բազմություն և կվերադարձնի ցուցակ,
 # որի էլեմենտները կլինեն բազմության էլեմենտների խորանարդները։
+def getListFromSetElCabs(numSet):
+    newList = []
+    for x in numSet:
+        newList.append(x ** 3)
+    return newList
+
 numSet = {1,2,3,4,5}
-newList = []
-for x in numSet:
-    newList.append(x**3)
-print("#7 ", newList)
+print("#7 ", getListFromSetElCabs(numSet))
--- src/first_month/Task_1_4_2.py
+++ src/first_month/Task_1_4_2.py
@@ -1,57 +1,79 @@
 #1. Գրել ծրագիր, որը տրված բառարանում կավելացնի նոր արժեք տրված բանալիով
+def addDictValByGivenKey(dict1, key, val):
+    dict1[key] = val
+    return dict1
+
 dict1 = { "brand": "Ford",  "model": "Mustang",  "year": 1964}
-dict1["color"] = "red"
-print('#1 ', dict1)
+print('#1 ', addDictValByGivenKey(dict1, 'color', 'red'))
 
 #2. Գրել ծրագիր, որը կմիավորի տրված 2 բառարանները
+def concatDicts(d1, d2):
+    d1.update(d2)
+    return d1
+
 d1 = {1:'a', 2: 'b'}
 d2 = {3: 'c', 4: 'd', 5: 'e'}
-d1.update(d2)
-print('#2 ', d1)
+print('#2 ', concatDicts(d1, d2))
 
 #3. Գրել ծրագիր, որը տրված n թվի համար կկառուցի բառարան,  որի բանալիները կլինեն 1֊ից n թվերը, իսկ արժեքները այդ թվերի քառակուսիները
-d3 = {}
-n = 10
-for i in range(1, n):
-    d3[i] = i**2
-print("#3 ", d3)
+def createDict(n):
+    d = {}
+    for i in range(1, n+1):
+        d[i] = i ** 2
+    return d
+
+print("#3 ", createDict(10))
 
 #4. Գրել ծրագիր, որը տրված նույն երկարությամբ ցուցակներից կկառուցի բառարան՝ առաջինը օգտագործելով որպես բանալիների բաազմություն, երկրորդը՝ արժեքների
+def createDictFromLists(d1, d2):
+    if len(d1) != len(d2):
+        return "Lists lengths must be the same"
+    dict1 = {}
+    for i in range(len(d1)):
+        dict1[d1[i]] = d2[i]
+    return dict1
+
 d1 = [1, 2, 3, 4]
 d2 = ['a', 'b', 'c', 'd']
-dict1 = {}
-for i in range(len(d1)):
-    dict1[d1[i]] = d2[i]
-print("#4 ", dict1)
+print("#4 ", createDictFromLists(d1, d2))
 
 #5. Գրել ծրագիր, որը տրված բառարանի համար կվերադարձնի ամենամեծ և ամենափոքր արժեքները
+def getDictMinAndMaxVals(dict1):
+    l1 = list(dict1.values())
+    l1.sort()
+    return "Min: "+str(l1[0]), "Max: "+str(l1[-1])
+
 dict1 = {1: 25, 3: 14, 4: 33, 2: 11, 0: 55}
-l1 = list(dict1.values())
-l1.sort()
-print("#5  Min: ", l1[0], '; Max: ',l1[-1] )
+print("#5 ", getDictMinAndMaxVals(dict1) )
 
 #6. Գրել ծրագիր, որը տրված 2 բառարանները կմիավորի իրար, այնպես, որ եթե 2֊ում էլ կան նույն բանալիով էլեմենտներ,
 # ապա միավորված բառարանի համապատասխան էլեմենտը կլինի տրված բանալիով և որպես արժեք կընդունի այդ 2 էլեմենտների արժեքների գումարը։
+def combineDicts(dict1, dict2):
+    LongSet = dict1 if len(dict1) > len(dict2) else dict2
+    ShortSet = dict1 if len(dict1) < len(dict2) else dict2
+    for k, v in LongSet.items():
+        if k in ShortSet:
+            newVal = int(dict1[k]) + int(dict2[k])
+            dict1.update({k: newVal})
+        else:
+            dict1.update({k: v})
+    return dict1
+
 dict1 = {0: 55, 1: 25, 2: 11 }
 dict2 = {0: 5, 1: 15, 5: 33, 6: 11, 7: 3, 8: 125}
-LongSet = dict1 if len(dict1)>len(dict2) else dict2
-ShortSet = dict1 if len(dict1)<len(dict2) else dict2
-for k, v in LongSet.items():
-    if k in ShortSet:
-        newVal = int(dict1[k])+int(dict2[k])
-        dict1.update({k: newVal})
-    else:
-        dict1.update({k: v})
-print("#6 ", dict1)
+print("#6 ", combineDicts(dict1, dict2))
 
 #7. Գրել ծրագիր, որը տրված տողի համար կկառուցի բառարան, այնպես, որ բառարանի բանալիները կլինեն տրված տողի սիմվոլները,
 # իսկ համապատասխան արժեքները կլինեն տողում այդ սիմվոլի քանակը։
+def getDictFromStrElmsAndCount(str1):
+    set1 = set(str1)
+    newDict = {}
+    for x in set1:
+        newDict[x] = str1.count(x)
+    return newDict
+
 str1 = "aaabbcdeeee"
-set1 = set(str1)
-newDict = {}
-for x in set1:
-    newDict[x] = str1.count(x)
-print("#7 ", newDict)
+print("#7 ", getDictFromStrElmsAndCount(str1))
 
 
 

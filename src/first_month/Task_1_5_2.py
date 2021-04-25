b27ea08a3728a377cd07a77292ea38de1c72317e
ElmiraSinani <elmira.si19@gmail.com>
2021-04-25 11:04:53 -0700

    updated conversion method

--- src/first_month/Task_1_5_2.py
+++ src/first_month/Task_1_5_2.py
@@ -18,21 +18,21 @@ class Money:
     def sub(self, obj2):
         return Money(self.amount-obj2.amount, self.currency)
 
-    def conversion(self, convert_from, convert_to, amount):
-        amount = amount/self.rates[convert_from] * self.rates[convert_to]
-        return amount
+    def conversion(self, convert_from, convert_to):
+        converted_amount = self.amount/self.rates[convert_from] * self.rates[convert_to]
+        return converted_amount
 
 def main():
 
-    obj1 = Money(250, 'USD')
+    obj1 = Money(100, 'USD')
     obj2 = Money(50, 'USD')
     print("obj1: ", obj1.print_obj())
     print("obj2: ", obj2.print_obj())
     print("obj1+obj2: ", obj1.sum(obj2).print_obj())
     print("obj1-obj2: ", obj1.sub(obj2).print_obj())
 
-    print(obj1.conversion("RUB", "AMD", 1000))
-    print(obj1.conversion("USD", "AMD", 150))
-    print(obj1.conversion("AMD", "AMD", 150))
+    print(obj1.conversion("RUB", "AMD"))
+    print(obj1.conversion("USD", "AMD"))
+    print(obj1.conversion("AMD", "AMD"))
 
 main()

55a1fc41e89f15f41df6e4153f9a7118929efda5
ElmiraSinani <elmira.si19@gmail.com>
2021-04-24 21:42:26 -0700

    RockPaperScissors updated

--- src/extra/rockPaperScissors.py
+++ src/extra/rockPaperScissors.py
@@ -1,8 +1,43 @@
 import random
 
 options = ["Rock", "Paper", "Scissors"]
-randomNum = random.randint(0,2)
-randomOption = options[randomNum]
 
-print(randomNum)
-print(randomOption)
+attempts = 0
+playerScore = 0
+compScore = 0
+maxAttempts = 3  #don't count tie an wrong typed options
+while attempts<maxAttempts:
+    inputedOption = input("Please type one of this options: Rock, Paper or Scissors\n")
+    randomNum = random.randint(0, 2)
+    randomOption = options[randomNum]
+    attempts += 1
+    if inputedOption == randomOption:
+        print("Tie! Equal options. Try again.")
+        attempts -= 1
+    elif inputedOption == 'Rock':
+        if randomOption == 'Paper':
+            print("You win!", "Random Option was ", randomOption)
+            playerScore += 1
+        else:
+            print("You lose!", "Random Option was ", randomOption)
+            compScore += 1
+    elif inputedOption == 'Paper':
+        if randomOption == 'Rock':
+            print("You win!", "Random Option was ", randomOption)
+            playerScore += 1
+        else:
+            print("You lose!", "Random Option was ", randomOption)
+            compScore += 1
+    elif inputedOption == 'Scissors':
+        if randomOption == 'Paper':
+            print("You win!", "Random Option was ", randomOption)
+            playerScore += 1
+        else:
+            print("You lose!", "Random Option was ", randomOption)
+            compScore += 1
+    else:
+        print("Wrong option. Please input your option again")
+        attempts -= 1
+
+print("\n\nScore-> You: ", playerScore, "; Comp: ", compScore)
+

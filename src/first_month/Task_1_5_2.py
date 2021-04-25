dc9858f681928f77504ba470e9017cedeec06fb8
ElmiraSinani <elmira.si19@gmail.com>
2021-04-25 10:39:52 -0700

    Added conversion method to Money class

--- src/first_month/Task_1_5_2.py
+++ src/first_month/Task_1_5_2.py
@@ -1,4 +1,10 @@
+import requests
+
 class Money:
+    api_url = "https://api.exchangerate-api.com/v4/latest/AMD"
+    json_data = requests.get(api_url).json()
+    rates = json_data['rates']
+
     def __init__(self, amount, currency):
         self.amount = amount
         self.currency = currency
@@ -12,13 +18,21 @@ class Money:
     def sub(self, obj2):
         return Money(self.amount-obj2.amount, self.currency)
 
+    def conversion(self, convert_from, convert_to, amount):
+        amount = amount/self.rates[convert_from] * self.rates[convert_to]
+        return amount
 
 def main():
+
     obj1 = Money(250, 'USD')
     obj2 = Money(50, 'USD')
     print("obj1: ", obj1.print_obj())
     print("obj2: ", obj2.print_obj())
     print("obj1+obj2: ", obj1.sum(obj2).print_obj())
     print("obj1-obj2: ", obj1.sub(obj2).print_obj())
+
+    print(obj1.conversion("RUB", "AMD", 1000))
+    print(obj1.conversion("USD", "AMD", 150))
+    print(obj1.conversion("AMD", "AMD", 150))
 
 main()

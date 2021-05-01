4512f0744f0d2e0d9f5aae68ea487de40479be7b
ElmiraSinani <elmira.si19@gmail.com>
2021-05-01 15:01:59 -0700

    Rational Class

---
+++ src/first_month/Task_1_6_1_OOP_Rational_Class_magic_Functions.py
@@ -0,0 +1,81 @@
+class Rational:
+
+    def __init__(self, x, y):
+        self.nominator = x
+        self.denominator = y
+
+    @staticmethod
+    def lcm(a, b):
+        return abs(a * b) // Rational.gcd(a, b)
+
+    @staticmethod
+    def gcd(a, b):
+        if b==0:
+            return a
+        else:
+            return Rational.gcd(b, a%b)
+
+    def __repr__(self):
+        return "nominator: "+str(self.nominator)+"; denominator: "+str(self.denominator)
+
+    def __add__(self, other):
+        x = self.lcm(self. denominator, other.denominator)
+        y = self.nominator*(x/self.denominator)+other.nominator*(x/other.denominator)
+        return Rational(x,y)
+
+    def __sub__(self, other):
+        x = self.lcm(self.denominator, other.denominator)
+        y = self.nominator * (x / self.denominator) - other.nominator * (x / other.denominator)
+        return Rational(x, y)
+
+    def __mul__(self, other):
+        x = self.nominator*other.nominator
+        y = self.denominator*other.denominator
+        return Rational(x, y)
+
+    def __div__(self, other):
+        x = self.nominator * other.denominator
+        return Rational(x, y)
+
+    def __eq__(self, other):
+        if self.nominator/self.denominator == other.nominator/other.denominator:
+            return True
+        return False
+
+    def __gt__(self, other):
+        if self.nominator / self.denominator > other.nominator / other.denominator:
+            return True
+        return False
+
+    def __ge__(self, other):
+        if self.nominator / self.denominator >= other.nominator / other.denominator:
+            return True
+        return False
+
+    def __lt__(self, other):
+        if self.nominator / self.denominator < other.nominator / other.denominator:
+            return True
+        return False
+
+    def __le__(self, other):
+        if self.nominator / self.denominator <= other.nominator / other.denominator:
+            return True
+        return False
+
+    def __pow__(self, power, modulo=None):
+        pass
+
+
+
+r1 = Rational(5, 8)
+r2 = Rational(3, 2)
+
+#print (Rational.gcd(54,24))
+#print (r2)
+print ("__add__ ",  r1+r2)
+print ("__sub__ ",  r1-r2)
+print ("__mul__ ",  r1*r2)
+print ("__dev__ ",  r1/r2)
+print ("__eq__ ",  r1==r2)
+
+

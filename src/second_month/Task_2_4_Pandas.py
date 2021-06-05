15d829f9a81cfd9457fbcabe2337b8d2f94a9a14
ElmiraSinani <elmira.si19@gmail.com>
2021-06-05 11:38:09 -0700

    updated function from print to return

--- src/second_month/Task_2_4_Pandas.py
+++ src/second_month/Task_2_4_Pandas.py
@@ -5,58 +5,56 @@ import pandas as pd
 
 # 1. Write a Pandas program to add, subtract, multiple and divide two Pandas Series
 def series_basic_actions(s1, s2):
-    print("#1. Add two Series: \n", s1 + s2)
-    print("#1. Subtract two Series: \n", s1 - s2)
-    print("#1. Multiply two Series: \n", s1 * s2)
-    print("#1. Divide Series1 by Series2: \n", s1 / s2)
+    return "\n Add two Series: \n {} \n Subtract two Series: \n {} \nMultiply  two Series\n {} \n Divide Series1 by " \
+           "Series2 \n {}".format(s1 + s2, s1 - s2, s1 * s2, s1 / s2)
 
 
 # 2. Write a Pandas program to convert a dictionary to a Pandas series.
 def convert_dic_to_series(d):
-    print("#2 convert dict to series: \n", pd.Series(d))
+    return pd.Series(d)
 
 
 # 3. Write a Pandas program to convert a NumPy array to a Pandas series
-def convert_np_arr_to_series(s):
-    print("#3 convert np array to series: \n", s)
+def convert_np_arr_to_series(np_arr):
+    obj = pd.Series(np_arr)
+    return obj
 
 
 # 4. Write a Pandas program to convert the first column of a DataFrame as a Series
 def convert_df_column_to_series(df):
-    print("#4 convert first column of a DataFrame as a Series: \n", pd.Series(df['A']))
+    return pd.Series(df.iloc[0])
 
 
 # 5. Write a Pandas program to sort a given Series
 def sort_series(s):
-    print("# 5 Sort given Series: \n", s.sort_index())
+    return s.sort_index()
 
 
 # 6 Write a Pandas program to select the rows the score is between 15 and 20 (inclusive)
 def select_rows_score(exam_data):
     df = pd.DataFrame(exam_data)
-    print("# 6 Score between 15 and 20\n", df[df['score'].between(15, 20)])
+    return df[df.iloc[:, 1].between(15, 20)]
 
 
 # 7 Write a Pandas program to calculate the sum of the examination attempts by the students.
 def sum_examination_attempts(exam_data):
     df = pd.DataFrame(exam_data)
-    print("# 7 sum of the examination attempts\n", df['attempts'].sum())
-
+    return df.iloc[:, 2].sum()
 
 
 def main():
     # 1
     s1 = pd.Series([2, 4, 6, 8, 10])
     s2 = pd.Series([1, 3, 5, 7, 9])
-    series_basic_actions(s1, s2)
+    print("#1 Series basic actions", series_basic_actions(s1, s2))
 
     # 2
     d = {"brand": "Ford", "model": "Mustang", "year": 1964}
-    convert_dic_to_series(d)
+    print("#2 convert dict to series: \n", convert_dic_to_series(d))
 
     # 3
-    obj = pd.Series(np.arange(4.), index=['a', 'b', 'c', 'd'])
-    convert_np_arr_to_series(obj)
+    np_arr = np.arange(4.)
+    print("#3 convert np array to series: \n", convert_np_arr_to_series(np_arr))
 
     # 4
     df = pd.DataFrame({"A": pd.Categorical(["Apple", "Banana", "Blueberries", "Dewberry"]),
@@ -64,11 +62,11 @@ def main():
                        "C": pd.Series(1, index=list(range(4)), dtype="float32"),
                        "D": np.array([3] * 4, dtype="int32"),
                        })
-    convert_df_column_to_series(df)
+    print("#4 convert first column of a DataFrame as a Series: \n", convert_df_column_to_series(df))
 
     # 5
     obj1 = pd.Series(range(4), index=['a', 'c', 'g', 'b'])
-    sort_series(obj1)
+    print("# 5 Sort given Series: \n",  sort_series(obj1))
 
     # 6
     exam_data = {
@@ -77,10 +75,10 @@ def main():
         'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
         'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']
     }
-    select_rows_score(exam_data)
+    print("# 6 Score between 15 and 20\n", select_rows_score(exam_data))
 
     # 7
-    sum_examination_attempts(exam_data)
+    print("# 7 sum of the examination attempts\n", sum_examination_attempts(exam_data))
 
 
 main()
--- src/second_month/Task_2_5_Pandas_read_files.py
+++ src/second_month/Task_2_5_Pandas_read_files.py
@@ -5,48 +5,48 @@ import pandas as pd
 
 # 1. Write a Pandas program to display all the records of REGIONS file
 def read_from_file(file):
-    print("#1: Show All Region Info \n", pd.read_csv(file))
+    return pd.read_csv(file)
 
 
 # 2. Write a Pandas program to display all the location id from locations file.
-def get_location_column(file, column_name):
+def get_location_column(file, col_name):
     locations = pd.read_csv(file)
-    print("#2: Display location ids \n", locations[['ID']])
+    return locations[[col_name]]
 
 
 # 3. Write a Pandas program to extract first 7 records from employees file.
 def extract_sub_records(file, rows_number):
-    print("#3: extract sub records \n", pd.read_csv(file, nrows=rows_number))
+    return pd.read_csv(file, nrows=rows_number)
 
 
 # 4. Write a Pandas program to select distinct department id from employees file.
 def select_distinct_dep_ids(file):
     f = pd.read_csv(file)
-    print("#4. distinct department ids: \n", f.DepartmentID.unique())
+    return f.DepartmentID.unique()
 
 
 # 5. Write a Pandas program to display the first, last name, salary and department number for those employees
 # whose first name starts with the letter 'S'.
 def filter_emp_data(file, start_with_letter):
     df = pd.read_csv(file, usecols=['FirstName', 'LastName', 'Salary', 'DepartmentID'])
-    print("# 5 \n", df[df.FirstName.str.startswith(start_with_letter)])
+    return df[df.FirstName.str.startswith(start_with_letter)]
 
 
 def main():
     # 1
-    read_from_file("files/us_regions_and_divisions.csv")
+    print("#1: Show All Region Info \n", read_from_file("files/us_regions_and_divisions.csv"))
 
     # 2
-    get_location_column("files/locations.csv", 'ID')
+    print("#2: Display location ids \n", get_location_column("files/locations.csv", 'ID'))
 
     # 3
-    extract_sub_records("files/employees.csv", 7)
+    print("#3: extract sub records \n", extract_sub_records("files/employees.csv", 7))
 
     # 4
-    select_distinct_dep_ids("files/employees.csv")
+    print("#4. distinct department ids: \n", select_distinct_dep_ids("files/employees.csv"))
 
     # 5
-    filter_emp_data("files/employees.csv", 'A')
+    print("# 5 \n", filter_emp_data("files/employees.csv", 'A'))
 
 
 main()
